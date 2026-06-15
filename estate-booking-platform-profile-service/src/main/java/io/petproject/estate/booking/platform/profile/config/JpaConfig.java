0ea7a10dae69b9ad92fa36818309442ee58b862f
Alexander Dudkin <raddanprofile@gmail.com>
2026-06-15 14:08:05 +0300

    #23: Implement Profile Service JPA Entities

---
+++ estate-booking-platform-profile-service/src/main/java/io/petproject/estate/booking/platform/profile/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package io.petproject.estate.booking.platform.profile.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
---
+++ estate-booking-platform-profile-service/src/main/java/io/petproject/estate/booking/platform/profile/entity/enums/VerificationStatus.java
@@ -0,0 +1,7 @@
+package io.petproject.estate.booking.platform.profile.entity.enums;
+
+public enum VerificationStatus {
+    PENDING,
+    VERIFIED,
+    REJECTED
+}
