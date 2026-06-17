353ae4a8e7adb2405ec48a5a2f09495d17f8303f
Alexander Dudkin <raddanprofile@gmail.com>
2026-06-17 21:01:15 +0300

    #45: Create Maven module Identity API with initial dependencies

---
+++ estate-booking-platform-identity-api/src/main/java/io/petproject/estate/booking/platform/identity/IdentityApiApplication.java
@@ -0,0 +1,11 @@
+package io.petproject.estate.booking.platform.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityApiApplication.class, args);
+    }
+}
