e6f4c29b51effeb56ac8665e4b4b04a765f3d06c
Alexander Dudkin <raddanprofile@gmail.com>
2026-07-02 02:16:29 +0300

    Fix

--- profile-service/src/main/java/io/petproject/estate/booking/platform/profile/ProfileServiceApplication.java
+++ profile-service/src/main/java/io/petproject/estate/booking/platform/profile/ProfileServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfileServiceApplication {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(ProfileServiceApplication.class, args);
     }
 }
